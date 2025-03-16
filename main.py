6d1925220adcab53a6d53e08ddd4492871741919
collins <146512547+Collins030@users.noreply.github.com>
2025-03-16 19:57:29 +0300

    Update main.py
    fixing recommendation issue

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 #import communityfoumupdate  # Import your community page
-import recommendation #import recommendation page
+import pages.recommendation #import recommendation page
 
 # Tensorflow Model Prediction
 # Tensorflow Model Prediction
