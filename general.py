621584d385fb376fc0d6e8fa225f7f5cf5f1c6a3
collins <146512547+Collins030@users.noreply.github.com>
2025-03-18 16:27:49 +0300

    Update general.py

--- general.py
+++ general.py
@@ -14,7 +14,7 @@ from io import BytesIO
 # Tensorflow Model Prediction
 # Tensorflow Model Prediction
 def model_prediction(test_image):
-    model = tf.keras.models.load_model("C:/Users/User/Documents/python_venv/model/trained_plant_disease_model2.keras")
+    model = tf.keras.models.load_model("model/trained_plant_disease_model2.keras")
     image = tf.keras.preprocessing.image.load_img(test_image, target_size=(128, 128))
     input_arr = tf.keras.preprocessing.image.img_to_array(image)
     input_arr = np.array([input_arr])  # Convert single image to batch
